1a41280471a60fda01363df1560d29de0ef01367
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2021-08-05 15:42:42 +0300

    finished

--- lesson 1 -  Introduction to Algorithmization and Simple Algorithms in Python/lesson1_task4/l1t4.py
+++ lesson 1 -  Introduction to Algorithmization and Simple Algorithms in Python/lesson1_task4/l1t4.py
@@ -2,13 +2,12 @@
 4. Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят, и сколько между ними находится букв.
 """
 
-chars = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split()
-
 first = input('Введите первую букву: ')
 second = input('Введите вторую букву: ')
 
-idx_first = chars.index(first) + 1
-idx_second = chars.index(second) + 1
+idx_first = ord(first) - ord('a') + 1
+idx_second = ord(second) - ord('a') + 1
+
 print(f'{first} - {idx_first} буква в алфавите\n'
       f'{second} - {idx_second} буква в алфавите')
 
