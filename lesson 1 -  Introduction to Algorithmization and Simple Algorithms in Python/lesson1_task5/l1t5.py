a77d8fb69f3f564e8ce411830ce87b5457e91aca
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2021-08-05 15:46:59 +0300

    finished

--- lesson 1 -  Introduction to Algorithmization and Simple Algorithms in Python/lesson1_task5/l1t5.py
+++ lesson 1 -  Introduction to Algorithmization and Simple Algorithms in Python/lesson1_task5/l1t5.py
@@ -2,13 +2,7 @@
 5. Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
 """
 
-chars = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split()
+char = input('Введите номер буквы: ')
+idx_char = ord('a') + int(char) - 1
 
-first = input('Введите номер первой буквы: ')
-second = input('Введите номер второй буквы: ')
-
-idx_first = int(first) - 1
-idx_second = int(second) - 1
-
-print(f'{first} буква - {chars[idx_first]}\n'
-      f'{second} буква - {chars[idx_second]}')
+print(f'{char} буква - {chr(idx_char)}\n')
