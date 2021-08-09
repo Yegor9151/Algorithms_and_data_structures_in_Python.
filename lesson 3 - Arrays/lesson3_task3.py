5a73d38dec12bcc5762bd19c21e3edf4bb875063
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2021-08-09 12:41:57 +0300

    finished

--- lesson 3 - Arrays/lesson3_task3.py
+++ lesson 3 - Arrays/lesson3_task3.py
@@ -8,20 +8,14 @@ import random
 A = [random.randint(0, 100) for _ in range(10)]
 print(A)
 
-min_a = A[0]
-max_a = A[0]
-for i in A:
-    if min_a > i:
-        min_a = i
-    if max_a < i:
-        max_a = i
-print(min_a, max_a)
-
-# General
-B = A[:]
+min_i = 0
+max_i = 0
 for i in range(len(A)):
-    if B[i] == min_a:
-        A[i] = max_a
-    if B[i] == max_a:
-        A[i] = min_a
+    if A[min_i] > A[i]:
+        min_i = i
+    if A[max_i] < A[i]:
+        max_i = i
+print(A[min_i], A[max_i])
+
+A[min_i], A[max_i] = A[max_i], A[min_i]
 print(A)
